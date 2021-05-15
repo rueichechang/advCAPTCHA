77d6f9f06daba54e8ec344606f14b15e4aa013b3
rueichechang <james304088@gmail.com>
2021-05-15 21:52:44 +0800

    replace instructions with chinese version

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ my_client = pymongo.MongoClient(mongo_url)
 mydb = my_client["advcaptcha"]
 task_data = mydb["task"]
 demographic_data = mydb["demographic"]
+post_question_collection = mydb["post_question_collection"]
 preference_data = mydb["preference"]
 
 # global participants_num 
@@ -48,7 +49,22 @@ socketio = SocketIO(app, async_mode=async_mode)
 #         groupA_count[name] = 0
 #         groupB_count[name] = 0
 #         groupC_count[name] = 0
+@app.route('/post_question',methods=['POST'])
+def post_question():
+    name = request.values['name']
+    email = request.values['email']
+    reasontolove = request.values['reasontolove']
+    reasontohate = request.values['reasontohate']
 
+    result = {}
+    result['name'] = name
+    result['email'] = email
+    result['reasontolove'] = reasontolove
+    result['reasontohate'] = reasontohate
+
+    post_question_collection.insert_one(result)
+
+    return render_template('thank.html',**locals())
 @app.route('/submit',methods=['POST'])
 def submit():
     print("======== submit success ============")
@@ -58,6 +74,7 @@ def submit():
     group = request.values['group']
     task_order = request.values['task_order']
 
+    print("======== 1 ============")
     time_0 = request.values['0']
     time1_1 = request.values['11']
     time1_2 = request.values['12']
@@ -71,6 +88,7 @@ def submit():
     time4_1 = request.values['41']
     time4_2 = request.values['42']
     time4_3 = request.values['43']
+    print("======== 2 ============")
 
     count_0 = request.values['c0']
     count1_1 = request.values['c11']
@@ -85,6 +103,7 @@ def submit():
     count4_1 = request.values['c41']
     count4_2 = request.values['c42']
     count4_3 = request.values['c43']
+    print("======== 3 ============")
 
     user_input0 = request.values['pro0']
     user_input1_1 = request.values['pro1_1']
@@ -99,6 +118,7 @@ def submit():
     user_input4_1 = request.values['pro4_1']
     user_input4_2 = request.values['pro4_2']
     user_input4_3 = request.values['pro4_3']
+    print("======== 4 ============")
 
     ground_truth0 = request.values['ground_truth0']
     ground_truth1_1 = request.values['ground_truth1_1']
@@ -113,6 +133,7 @@ def submit():
     ground_truth4_1 = request.values['ground_truth4_1']
     ground_truth4_2 = request.values['ground_truth4_2']
     ground_truth4_3 = request.values['ground_truth4_3']
+    print("======== 5 ============")
 
     captcha_id0 = request.values['captcha_id0']
     captcha_id1_1 = request.values['captcha_id1_1']
@@ -127,6 +148,7 @@ def submit():
     captcha_id4_1 = request.values['captcha_id4_1']
     captcha_id4_2 = request.values['captcha_id4_2']
     captcha_id4_3 = request.values['captcha_id4_3']
+    print("======== 6 ============")
 
     file_address0 = request.values['file_address0']
     file_address1_1 = request.values['file_address1_1']
@@ -141,6 +163,7 @@ def submit():
     file_address4_1 = request.values['file_address4_1']
     file_address4_2 = request.values['file_address4_2']
     file_address4_3 = request.values['file_address4_3']
+    print("======== 7 ============")
 
     task_type0 = request.values['task_type0']
     task_type1_1 = request.values['task_type1_1']
@@ -155,31 +178,41 @@ def submit():
     task_type4_1 = request.values['task_type4_1']
     task_type4_2 = request.values['task_type4_2']
     task_type4_3 = request.values['task_type4_3']
+    print("======== 8 ============")
 
 
     likert_fb1_1 = request.values['likert_fb1_1']
     likert_fb1_2 = request.values['likert_fb1_2']
-    likert_fb1_3 = request.values['likert_fb1_3']
+    # likert_fb1_3 = request.values['likert_fb1_3']
     text_fb1_1 = request.values['text_fb1_1']
     text_fb1_2 = request.values['text_fb1_2']
+    text_fb1_3 = request.values['text_fb1_3']
+    print("======== 9 ============")
 
     likert_fb2_1 = request.values['likert_fb2_1']
     likert_fb2_2 = request.values['likert_fb2_2']
-    likert_fb2_3 = request.values['likert_fb2_3']
+    # likert_fb2_3 = request.values['likert_fb2_3']
     text_fb2_1 = request.values['text_fb2_1']
     text_fb2_2 = request.values['text_fb2_2']
+    text_fb2_3 = request.values['text_fb2_3']
+    print("======== 10 ============")
 
     likert_fb3_1 = request.values['likert_fb3_1']
     likert_fb3_2 = request.values['likert_fb3_2']
-    likert_fb3_3 = request.values['likert_fb3_3']
+    # likert_fb3_3 = request.values['likert_fb3_3']
     text_fb3_1 = request.values['text_fb3_1']
     text_fb3_2 = request.values['text_fb3_2']
+    text_fb3_3 = request.values['text_fb3_3']
+    print("======== 11 ============")
 
     likert_fb4_1 = request.values['likert_fb4_1']
     likert_fb4_2 = request.values['likert_fb4_2']
-    likert_fb4_3 = request.values['likert_fb4_3']
+    # likert_fb4_3 = request.values['likert_fb4_3']
     text_fb4_1 = request.values['text_fb4_1']
     text_fb4_2 = request.values['text_fb4_2']
+    text_fb4_3 = request.values['text_fb4_3']
+
+    print("======== 12 ============")
 
     dataJson = {}
 
@@ -187,28 +220,36 @@ def submit():
     dataJson["instance1_1"] = dict(table_id=table_id,participant_id=participant_id,group=group,task_order=task_order,file_address=file_address1_1,captcha_id=captcha_id1_1,task_type=task_type1_1,ground_truth=ground_truth1_1,time=time1_1, count=count1_1, user_input=user_input1_1)
     dataJson["instance1_2"] = dict(table_id=table_id,participant_id=participant_id,group=group,task_order=task_order,file_address=file_address1_2,captcha_id=captcha_id1_2,task_type=task_type1_2,ground_truth=ground_truth1_2,time=time1_2, count=count1_2, user_input=user_input1_2)
     dataJson["instance1_3"] = dict(table_id=table_id,participant_id=participant_id,group=group,task_order=task_order,file_address=file_address1_3,captcha_id=captcha_id1_3,task_type=task_type1_3,ground_truth=ground_truth1_3,time=time1_3, count=count1_3, user_input=user_input1_3)
-    dataJson["feedback1"]   = dict(likert_fb1_1=likert_fb1_1, likert_fb1_2=likert_fb1_2, likert_fb1_3=likert_fb1_3, user_input_fb1_1=text_fb1_1, text_fb1_2=text_fb1_2)
+    dataJson["feedback1"]   = dict(likert_fb1_1=likert_fb1_1, likert_fb1_2=likert_fb1_2, text_fb1_1=text_fb1_1, text_fb1_2=text_fb1_2, text_fb1_3=text_fb1_3)
     
     dataJson["instance2_1"] = dict(table_id=table_id,participant_id=participant_id,group=group,task_order=task_order,file_address=file_address2_1,captcha_id=captcha_id2_1,task_type=task_type2_1,ground_truth=ground_truth2_1,time=time2_1, count=count2_1, user_input=user_input2_1)
     dataJson["instance2_2"] = dict(table_id=table_id,participant_id=participant_id,group=group,task_order=task_order,file_address=file_address2_2,captcha_id=captcha_id2_2,task_type=task_type2_2,ground_truth=ground_truth2_2,time=time2_2, count=count2_2, user_input=user_input2_2)
     dataJson["instance2_3"] = dict(table_id=table_id,participant_id=participant_id,group=group,task_order=task_order,file_address=file_address2_3,captcha_id=captcha_id2_3,task_type=task_type2_3,ground_truth=ground_truth2_3,time=time2_3, count=count2_3, user_input=user_input2_3)
-    dataJson["feedback2"]   = dict(likert_fb2_1=likert_fb2_1, likert_fb2_2=likert_fb2_2, likert_fb2_3=likert_fb2_3, text_fb2_1=text_fb2_1, text_fb2_2=text_fb2_2)
+    dataJson["feedback2"]   = dict(likert_fb2_1=likert_fb2_1, likert_fb2_2=likert_fb2_2, text_fb2_1=text_fb2_1, text_fb2_2=text_fb2_2, text_fb2_3=text_fb2_3)
     
     dataJson["instance3_1"] = dict(table_id=table_id,participant_id=participant_id,group=group,task_order=task_order,file_address=file_address3_1,captcha_id=captcha_id3_1,task_type=task_type3_1,ground_truth=ground_truth3_1,time=time3_1, count=count3_1, user_input=user_input3_1)
     dataJson["instance3_2"] = dict(table_id=table_id,participant_id=participant_id,group=group,task_order=task_order,file_address=file_address3_2,captcha_id=captcha_id3_2,task_type=task_type3_2,ground_truth=ground_truth3_2,time=time3_2, count=count3_2, user_input=user_input3_2)
     dataJson["instance3_3"] = dict(table_id=table_id,participant_id=participant_id,group=group,task_order=task_order,file_address=file_address3_3,captcha_id=captcha_id3_3,task_type=task_type3_3,ground_truth=ground_truth3_3,time=time3_3, count=count3_3, user_input=user_input3_3)
-    dataJson["feedback3"]   = dict(likert_fb3_1=likert_fb3_1, likert_fb3_2=likert_fb3_2, likert_fb3_3=likert_fb3_3, text_fb3_1=text_fb3_1, text_fb3_2=text_fb3_2)
+    dataJson["feedback3"]   = dict(likert_fb3_1=likert_fb3_1, likert_fb3_2=likert_fb3_2, text_fb3_1=text_fb3_1, text_fb3_2=text_fb3_2, text_fb3_3=text_fb3_3)
     
     dataJson["instance4_1"] = dict(table_id=table_id,participant_id=participant_id,group=group,task_order=task_order,file_address=file_address4_1,captcha_id=captcha_id4_1,task_type=task_type4_1,ground_truth=ground_truth4_1,time=time4_1, count=count4_1, user_input=user_input4_1)
     dataJson["instance4_2"] = dict(table_id=table_id,participant_id=participant_id,group=group,task_order=task_order,file_address=file_address4_2,captcha_id=captcha_id4_2,task_type=task_type4_2,ground_truth=ground_truth4_2,time=time4_2, count=count4_2, user_input=user_input4_2)
     dataJson["instance4_3"] = dict(table_id=table_id,participant_id=participant_id,group=group,task_order=task_order,file_address=file_address4_3,captcha_id=captcha_id4_3,task_type=task_type4_3,ground_truth=ground_truth4_3,time=time4_3, count=count4_3, user_input=user_input4_3)
-    dataJson["feedback4"]   = dict(likert_fb4_1=likert_fb4_1, likert_fb4_2=likert_fb4_2, likert_fb4_3=likert_fb4_3, text_fb4_1=text_fb4_1, text_fb4_2=text_fb4_2)
+    dataJson["feedback4"]   = dict(likert_fb4_1=likert_fb4_1, likert_fb4_2=likert_fb4_2, text_fb4_1=text_fb4_1, text_fb4_2=text_fb4_2, text_fb4_3=text_fb4_3)
     
     task_data.insert_one(dataJson)
 
     # print(fname, lname, age, gender, email)
 
-    return render_template('submit.html',**locals())
+    return render_template('submit.html', async_mode=socketio.async_mode,
+                                            table_id = table_id,
+                                            participant_id = participant_id,
+                                            group = group,
+                                            task_order = task_order,
+                                            file_address1_1 = file_address1_1,
+                                            file_address2_1 = file_address2_1,
+                                            file_address3_1 = file_address3_1,
+                                            file_address4_1 = file_address4_1)
 
 
 @app.route('/demographic_control',methods=['POST'])
@@ -219,7 +260,11 @@ def demographic_control():
     table_id = request.values['table_id']
     age     = request.values['age']
     gender  = request.values['gender']
-    email   = request.values['email']
+    familiaritynum = request.values['familiaritynum']
+    familiaritycaptcha = request.values['familiaritycaptcha']
+    familiarityaudio = request.values['familiarityaudio']
+    apparatus = request.values['apparatus']
+    # email   = request.values['email']
 
     demographic = {}
     # demographic["fname"]    = fname
@@ -227,13 +272,17 @@ def demographic_control():
     demographic["table_id"]    = table_id
     demographic["age"]      = age
     demographic["gender"]   = gender
-    demographic["email"]    = email
+    demographic["familiaritynum"]   = familiaritynum
+    demographic["familiaritycaptcha"]   = familiaritycaptcha
+    demographic["familiarityaudio"]   = familiarityaudio
+    demographic["apparatus"]   = apparatus
+
+    # demographic["email"]    = email
     demographic_data.insert_one(demographic)
 
     # get group, task order
     group = "test group"
     task_order = " test task order"
-    # participants_num += 1 # TODO: should figure out how to add from global variable
     
     # get participants_num
     res = preference_data.update_one({"_id": 'participants_num'}, {"$inc": {"count": 1}}, upsert =True)
